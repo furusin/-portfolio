d2d25d11a04743c925e2358f7dc33a030711a0a6
furusin <furusin.oriver@gmail.com>
2021-09-17 22:32:59 +0900

    Add：Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/layout";
+import React from "react";
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <Box alignItems="center">
+        <text>©️ Shinji Furukawa</text>
+      </Box>
+    );
+  }
+}
